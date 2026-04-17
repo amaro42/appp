ae13b4af4fe5ce9f5db07abb7cfd31df4a98a168
amaro <amaro.cs24s@gmail.com>
2026-04-17 11:34:26 -0400

    ejercicio con for

--- a.py
+++ a.py
@@ -1,1 +1,19 @@
-input
+# for i in "amaro":
+#       print(i)
+#  # Pregunte al usuario su nombre u muestre sus letras 
+print ("ingrese su nombre")
+name=input()
+
+
+#nombra ("ingrese su nombre")
+nvocales=0
+nletras=0
+for i in name:
+        print(i)
+        nletras+=1
+        if i in "aeiouAEIOU":
+            nvocales+=1
+   elif: i==" ":
+print ("la cantidad de vocales de su nombre es", nvocales)
+print ("numero de letras de su nombre es", nletras)
+print ("la cantidad de consonantes de su nombre es", nletras-nvocales)
