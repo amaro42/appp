40a8084a4bfade318c2baac5f63279b45a231a05
amaro <amaro.cs24s@gmail.com>
2026-04-17 11:53:10 -0400

    ejerciocoo

--- a.py
+++ a.py
@@ -13,7 +13,34 @@ for i in name:
         nletras+=1
         if i in "aeiouAEIOU":
             nvocales+=1
-   elif: i==" ":
+        elif i==" ":
+            nletras-=1
+
 print ("la cantidad de vocales de su nombre es", nvocales)
 print ("numero de letras de su nombre es", nletras)
 print ("la cantidad de consonantes de su nombre es", nletras-nvocales)
+
+
+# preguntar la cantidad de votantes 
+# poner los dos candidatos: Dexter/samurai jack 
+# preguntar a cada votante por quien vota
+# al final mostrar el ganador o si hay empate
+
+print ("ingrese la cantidad de votantes")
+votantes=int(input())
+dexter=0
+samurai_jack=0
+for i in range (votantes):
+    print ("por quien vota? dexter o samurai jack")
+    voto=input()
+    if voto=="dexter":
+        dexter+=1
+    elif voto=="samurai jack":
+        samurai_jack+=1
+
+if dexter>samurai_jack:
+    print("el ganador es dexter")
+elif samurai_jack>dexter:
+    print("el ganador es samurai jack")
+else:
+    print("hay un empate")
